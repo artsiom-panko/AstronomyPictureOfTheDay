c468d56d3ca2bd140beb21fa4a0705581040fe73
artsiom.panko <artsiom.panko@gmail.com>
2022-12-17 23:42:52 +0200

    Controller and UI refactoring, added startup text

--- src/main/java/com/panko/astronomy_picture_of_the_day/controller/MainController.java
+++ src/main/java/com/panko/astronomy_picture_of_the_day/controller/MainController.java
@@ -3,16 +3,21 @@ package com.panko.astronomy_picture_of_the_day.controller;
 import com.panko.astronomy_picture_of_the_day.entity.Picture;
 import com.panko.astronomy_picture_of_the_day.service.MainService;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
-public class HelloController {
+public class MainController {
     MainService mainService = new MainService();
 
     @FXML
-    private Label welcomeText;
+    private Text welcomeText;
+
+    public void getImage() {
+        Picture picture = mainService.process();
+        welcomeText.setText(picture.getDescription());
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
+    public void initialize() {
         Picture picture = mainService.process();
         welcomeText.setText(picture.getDescription());
     }
