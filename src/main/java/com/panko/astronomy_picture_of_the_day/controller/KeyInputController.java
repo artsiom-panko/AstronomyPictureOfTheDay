c55df6ea447fc26409624d4cc736bab03ecc95ce
artsiom.panko <artsiom.panko@gmail.com>
2023-01-14 22:44:41 +0200

    A bit code refactoring

--- src/main/java/com/panko/astronomy_picture_of_the_day/controller/KeyInputController.java
+++ src/main/java/com/panko/astronomy_picture_of_the_day/controller/KeyInputController.java
@@ -1,10 +1,8 @@
 package com.panko.astronomy_picture_of_the_day.controller;
 
 import com.panko.astronomy_picture_of_the_day.util.ApplicationPropertiesManager;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.DirectoryChooser;
@@ -19,7 +17,7 @@ public class KeyInputController {
     @FXML
     private TextField selectedFolderDirectory;
     @FXML
-    private ToggleGroup language_group;
+    private ToggleGroup languageGroup;
 
     private Stage rootStage;
     private RootController rootController;
@@ -44,7 +42,7 @@ public class KeyInputController {
     private void saveNewApiKey() {
         boolean wasKeySavedSuccessfully = applicationPropertiesManager.saveKey("nasa.api.key", newApiKey.getText());
         applicationPropertiesManager.saveKey("pictures.folder", selectedFolderDirectory.getText());
-        applicationPropertiesManager.saveKey("language", ((RadioButton) language_group.getSelectedToggle()).getText());
+        applicationPropertiesManager.saveKey("language", ((RadioButton) languageGroup.getSelectedToggle()).getText());
 
         if (wasKeySavedSuccessfully) {
             rootController.process();
