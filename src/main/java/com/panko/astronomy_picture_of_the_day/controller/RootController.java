7c379c5238feca625f6e108282f1110e709a7669
artsiom.panko <artsiom.panko@gmail.com>
2023-02-24 22:41:44 +0200

    Fixed UI

--- src/main/java/com/panko/astronomy_picture_of_the_day/controller/RootController.java
+++ src/main/java/com/panko/astronomy_picture_of_the_day/controller/RootController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
@@ -34,8 +35,9 @@ public class RootController {
     private Stage primaryStage;
 
     @FXML
+    private HBox infoBlock;
+    @FXML
     private BorderPane rootContainer;
-
     @FXML
     private Text numberOfRocketLaunches;
 
@@ -67,6 +69,7 @@ public class RootController {
                 throw new RuntimeException(e);
             }
 
+            infoBlock.setVisible(false);
             showLaunchesCounter();
             rootContainer.setCenter(loadingScene);
 
@@ -84,6 +87,7 @@ public class RootController {
                         Platform.runLater(() -> {
                             loadPictureDescriptionScene(picture);
                             updateAndShowLaunchesCounter();
+                            infoBlock.setVisible(true);
                         });
                     }
                 } else {
